62956177612cd4b3c1f180b39b9d79f1f0686488
Jakub Nowakowski <contact@jnowakowski.com>
2017-12-12 18:20:36 +0100

    Base on ubuntu xenial box

--- Vagrantfile
+++ Vagrantfile
@@ -1,28 +1,29 @@
 Vagrant.configure("2") do |config|
+  config.vm.box = "ubuntu/xenial64"
+  config.ssh.insert_key = true
+  config.ssh.forward_agent = true
+
   config.vm.define "node1" do |node|
-    node.vm.box = "debian/jessie64"
-
-#        web.vm.network :forwarded_port, guest: 8080, host: 28080
     node.vm.network "private_network", ip: "192.168.10.21"
-    node.ssh.insert_key = false
 
+    node.vm.hostname = "node1"
+    
     node.vm.provision "shell", path: "bootstrap-kubeadm.sh"
   end
 
   config.vm.define "node2" do |node|
-    node.vm.box = "debian/jessie64"
-
     node.vm.network "private_network", ip: "192.168.10.22"
+
+    node.vm.hostname = "node2"
 
     node.vm.provision "shell", path: "bootstrap-kubeadm.sh"
   end
   
   config.vm.define "node3" do |node|
-    node.vm.box = "debian/jessie64"
-
     node.vm.network "private_network", ip: "192.168.10.23"
+
+    node.vm.hostname = "node3"
 
     node.vm.provision "shell", path: "bootstrap-kubeadm.sh"
   end
-
 end
--- bootstrap-kubeadm.sh
+++ bootstrap-kubeadm.sh
@@ -1,22 +1,24 @@
 #!/usr/bin/env bash
 
-sudo apt update
+echo "--------------- Installing prerequisites"
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 
-echo "Installing Docker"
-sudo apt install apt-transport-https ca-certificates curl gnupg2 software-properties-common
-curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | sudo apt-key add -
-sudo add-apt-repository \
-    "deb [arch=amd64] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") \
-    $(lsb_release -cs) \
-    stable"
+echo "--------------- Adding Docker repo"
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
-sudo apt install -y docker-ce
-sudo usermod -a -G docker $USER
-
-echo "Installing kubeadm, kubelet and kubectl"
-sudo apt install -y apt-transport-https
+echo "--------------- Adding Kubernetes repo"
 sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 sudo add-apt-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
-sudo apt update
-sudo apt install -y kubelet kubeadm kubectl
+
+echo "--------------- Updating repo"
+sudo apt-get update
+
+echo "--------------- Installing Docker"
+sudo apt-get install -y docker-ce=17.03.2~ce-0~ubuntu-xenial
+sudo usermod -a -G docker $USER
+
+echo "--------------- Installing kubeadm, kubelet and kubectl"
+sudo apt-get install -y kubelet kubeadm kubectl
 
