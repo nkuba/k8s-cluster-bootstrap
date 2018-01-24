0e49099e6ee58ca0df4c86fab85d4480bbc13c7b
Jakub Nowakowski <contact@jnowakowski.com>
2018-01-24 22:36:22 +0100

    Kubectl instalation script

---
+++ installKubeCtl.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+echo "Download latest kubectl"
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+
+echo "Configure kubectl autocompletion"
+echo "source <(kubectl completion bash)" >> ~/.bashrc
